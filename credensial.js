ff75018c1a6f55f2cd454fbf4ee9c41b70cc082a
Hitanshu697 <hitanshupanchal237@nhitm.ac.in>
2025-06-14 22:08:32 +0530

    new file added
    credensial

---
+++ credensial.js
@@ -0,0 +1,2 @@
+ const Iftemplateid = '1yY2xRt_jQ7a5YxKPClIRYToilTmZgj7_Rh-317W6hOg'; // Create a Google Doc template first and put its ID here
+  const Iffolderid = '1Gj9Pfd9A4SYjLxNuVLvEP40pX1c1uTyi';
