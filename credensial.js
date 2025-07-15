630d4fd46dfa0c86359d3bd2e605b8b1c3090471
Hitanshu697 <hitanshupanchal237@nhitm.ac.in>
2025-07-15 20:16:47 +0530

    new changes made
    admission form ( enrollment id , receipt)

--- credensial.js
+++ credensial.js
@@ -1,17 +1,17 @@
 
-const CONFIG = {                                                                    
-  ADMISSIONS_PDF_FOLDER_ID: '1z9SkGw0JXQmlj_NjLcK7Cal1Vvgctcwr',
+// const CONFIG = {                                                                    
+//   ADMISSIONS_PDF_FOLDER_ID: '1z9SkGw0JXQmlj_NjLcK7Cal1Vvgctcwr',
                                 
-  ADMISSIONS_SHEET_NAME: 'Admissions',                                              
-  INQUIRY_SHEET_NAME: 'DF',                                               
-  STUDENT_DATA_SHEET_NAME: 'STUDENT DATA',                                          
-  FEES_SHEET_NAME: 'FEES',                                                          
-  LOGIN_SHEET_NAME: 'LOGIN',                                                        
-  DROPDOWN_SHEET_NAME: 'DROPDOWN',                                              
-  AUDIT_LOG_SHEET_NAME: 'AuditLog',                                                 
-  RECEIPTS_SHEET_NAME: 'Receipts',                                                  
-  DEFAULT_USER: 'Anonymous',                                                        
-  ROLES: {                                                                          
-    ADMIN: 'admin'                                                                  
-  }                                                                                 
-};
+//   ADMISSIONS_SHEET_NAME: 'Admissions',                                              
+//   INQUIRY_SHEET_NAME: 'DF',                                               
+//   STUDENT_DATA_SHEET_NAME: 'STUDENT DATA',                                          
+//   FEES_SHEET_NAME: 'FEES',                                                          
+//   LOGIN_SHEET_NAME: 'LOGIN',                                                        
+//   DROPDOWN_SHEET_NAME: 'DROPDOWN',                                              
+//   AUDIT_LOG_SHEET_NAME: 'AuditLog',                                                 
+//   RECEIPTS_SHEET_NAME: 'Receipts',                                                  
+//   DEFAULT_USER: 'Anonymous',                                                        
+//   ROLES: {                                                                          
+//     ADMIN: 'admin'                                                                  
+//   }                                                                                 
+// };
